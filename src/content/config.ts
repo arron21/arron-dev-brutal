186903b6dbb16089b945007e5a3f7177504bd573
Arron McCrory <arronmccrory@gmail.com>
2024-11-25 11:49:15 -0500

    added optional to blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,11 +4,11 @@ const blogCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      author: z.string(),
+      author: z.string().optional(),
       tags: z.array(z.string()),
-      description: z.string(),
-      pubDate: z.string().transform((str) => new Date(str)),
-      imgUrl: image(),
+      description: z.string().optional(),
+      pubDate: z.string().transform((str) => new Date(str)).optional(),
+      imgUrl: image().optional(),
       draft: z.boolean().optional().default(false),
     }),
 });
